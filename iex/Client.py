1221f1be761f157d698a93a4f5b72c65f117189d
William Speirs <bill.speirs@gmail.com>
2017-08-20 10:34:11 -0400

    Added `get_price` method
    Returns the last traded price for a symbol or list of symbols

--- iex/Client.py
+++ iex/Client.py
@@ -1,18 +1,39 @@
 import requests
-import json
+
+from urllib.parse import quote_plus
 
 _BASE_URL = 'https://api.iextrading.com/1.0'
 
 
 class Client(object):
     def __init__(self):
-        res = requests.get(_BASE_URL + '/ref-data/symbols?filter=symbol,name')
+        self.session = requests.Session()  # setup a session for reuse
+        res = self.session.get(_BASE_URL + '/ref-data/symbols?filter=symbol,name')
 
         if res.status_code != 200:
             raise requests.RequestException(kwargs={'response': res})
 
+        # save off all the symbols the exchange knows about so we can reference later
         self.symbols = {x['symbol']: x['name'] for x in res.json()}
 
+    def get_price(self, stocks):
+        """
+        Gets the price of the last trade of a stock or list of stocks.
+        :param stocks: a single stock or list of stocks to fetch
+        :return: dict with uppercase symbols and prices, any unknown symbols are discarded
+        """
+        if not isinstance(stocks, list):
+            stocks = [stocks]  # create a list
 
+        # filter and quote the list
+        stocks = [quote_plus(s) for s in filter(lambda s: s in self.symbols, [str(s).upper() for s in stocks])]
+
+        if len(stocks) == 0:
+            return {}
+
+        # make the request, getting only symbol and price
+        res = self.session.get(_BASE_URL + '/tops/last?symbols=%s&filter=symbol,price'%','.join(stocks))
+
+        return {x['symbol']: x['price'] for x in res.json()}
 
 
--- test/ClientTest.py
+++ test/ClientTest.py
@@ -1,11 +1,23 @@
 import unittest
 
-from iex import client
+from iex.Client import Client
+
+c = Client()  # call this once so we can re-use sessions
 
 
 class ClientTest(unittest.TestCase):
-    def test__init__(self):
-        c = client.Client()
+    def test_get_price_list(self):
+        prices = c.get_price(['aapl', 'fb', 'zzaa'])
+
+        assert 'AAPL' in prices, 'AAPL not found'
+        assert 'FB' in prices, 'FB not found'
+        assert 'ZZAA' not in prices, 'ZZAA found'
+
+
+def test_get_price_single(self):
+    prices = c.get_price('aapl')
+
+    assert 'AAPL' in prices, 'AAPL not found'
 
 
 if __name__ == '__main__':
